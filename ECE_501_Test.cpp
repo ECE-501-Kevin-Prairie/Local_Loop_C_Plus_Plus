87a86fc2ad6a089c631dfaee4fe45108399f2844
Kevin Prairie <kevin.prairie16@gmail.com>
2021-02-19 08:30:56 -0500

    Add project files.

---
+++ ECE_501_Test.cpp
@@ -0,0 +1,107 @@
+// ECE_501_Test.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <mmsystem.h>
+#include <locale>
+#include <codecvt>
+
+#pragma comment ( lib, "winmm.lib" )
+
+using namespace std;
+
+class recorder
+{
+public:
+	void start()
+	{
+		paused = rec = false; action = "IDLE";
+		while (true)
+		{
+			cout << endl << "==" << action << "==" << endl << endl;
+			cout << "1) Record" << endl << "2) Play" << endl << "3) Pause" << endl << "4) Stop" << endl << "5) Quit" << endl;
+			char c; cin >> c;
+			if (c > '0' && c < '6')
+			{
+				switch (c)
+				{
+				case '1': record(); break;
+				case '2': play();   break;
+				case '3': pause();  break;
+				case '4': stop();   break;
+				case '5': stop();   return;
+				}
+			}
+		}
+	}
+private:
+	void record()
+	{
+		if (mciExecute("open new type waveaudio alias my_sound"))
+		{
+			mciExecute("record my_sound");
+			action = "RECORDING"; rec = true;
+		}
+	}
+	void play()
+	{
+		if (paused)
+			mciExecute("play my_sound");
+		else
+			if (mciExecute("open tmp.wav alias my_sound"))
+				mciExecute("play my_sound");
+
+		action = "PLAYING";
+		paused = false;
+	}
+	void pause()
+	{
+		if (rec) return;
+		mciExecute("pause my_sound");
+		paused = true; action = "PAUSED";
+	}
+	void stop()
+	{
+		if (rec)
+		{
+			mciExecute("stop my_sound");
+			mciExecute("save my_sound tmp.wav");
+			mciExecute("close my_sound");
+			action = "IDLE"; rec = false;
+		}
+		else
+		{
+			mciExecute("stop my_sound");
+			mciExecute("close my_sound");
+			action = "IDLE";
+		}
+	}
+	bool mciExecute(string cmd)
+	{
+		std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+
+		std::wstring test = converter.from_bytes(cmd);
+		
+		if (mciSendString(test.c_str(), NULL, 0, NULL))
+		{
+			cout << "Can't do this: " << cmd << endl;
+			return false;
+		}
+		return true;
+	}
+
+	bool paused, rec;
+	string action;
+};
+
+int main()
+{
+   // std::cout << "Playing Audio...\n";
+   // PlaySound(TEXT("StarWars60.wav"), GetModuleHandle(NULL), SND_FILENAME);
+   // std::cout << "Audio Ended...\n";
+	
+	recorder r; r.start();
+	return 0;
+}
